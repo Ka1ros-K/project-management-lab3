541dae130c31ad233d35ee8289ba2450c313cdfa
Ka1ros-K <1845394872@qq.com>
2026-05-30 21:05:47 +0800

    功能更新：main.py调用utils工具函数

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 # 主程序文件
+from utils import add, subtract
+
 def main():
     print("Hello, Git!")
+    print(f"1 + 2 = {add(1, 2)}")
+    print(f"5 - 3 = {subtract(5, 3)}")
 
 if __name__ == "__main__":
     main()
