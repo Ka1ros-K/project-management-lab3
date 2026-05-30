0e103a54677dea8ddbaf4cdfebe0fedd5d286826
Ka1ros-K <1845394872@qq.com>
2026-05-30 21:07:38 +0800

    功能扩展：添加除法函数

--- utils.py
+++ utils.py
@@ -1,6 +1,12 @@
-# 工具函数
+# 工具函数库
 def add(a, b):
     return a + b
 
 def subtract(a, b):
     return a - b
+
+def divide(a, b):
+    """除法函数"""
+    if b == 0:
+        raise ValueError("除数不能为零")
+    return a / b
