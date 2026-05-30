6bb09d8d12df884197f99186fca9831439402de7
Ka1ros-K <1845394872@qq.com>
2026-05-31 02:35:30 +0800

    初始提交：创建utils基础文件

--- utils.py
+++ utils.py
@@ -1,18 +1,1 @@
 # 工具函数库
-def add(a, b):
-    """加法函数"""
-    return a + b
-
-def subtract(a, b):
-    """减法函数"""
-    return a - b
-
-def multiply(a, b):
-    """乘法函数"""
-    return a * b
-
-def divide(a, b):
-    """除法函数"""
-    if b == 0:
-        raise ValueError("除数不能为零")
-    return a / b
